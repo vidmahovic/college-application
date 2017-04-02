cb73cc43d0431bbd11ec1185343a14df3ea88edc
Rok Zidarn <rz3817@student.uni-lj.si>
2017-04-02 13:09:41 +0200

    Added Fractal, modified controller, needs scopes

--- app/FacultyProgram.php
+++ app/FacultyProgram.php
@@ -20,6 +20,42 @@ class FacultyProgram extends Model
         'min_points' => 'integer'
     ];
 
+    /*
+    public function scopeIncomplete($query){ // scope prefix!
+        // with scope you can chain methods, such as where()
+        return $query->where('completed', 0);
+    }
 
+    public function scopeFilter($query, $filters){
+        if($month = $filters['month']){
+            $query->whereMonth('created_at', Carbon::parse($month)->month);
+        }
+
+        if($year = $filters['year']){
+            $query->whereYear('created_at', Carbon::parse($year)->year);
+        }
+
+        // return $query->get();
+    }
+
+    public static function archives()
+    {
+        return selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
+            ->groupBy('year', 'month')->orderByRaw('min(created_at) desc')->get()->toArray();
+    }
+    */
+
+    /*
+    $newContacts = DB::table('contact')->where('created_at', '>', Carbon::now()->subDay())->get();
+    $mediumDrinks = DB::table('drinks')->whereBetween('size', [6, 12])->distinct()->get();
+    $contacts = DB::table('contacts') >orderBy('last_name', 'asc')->get();
+    $page4 = DB::table('contacts')->skip(30)->take(10)->get();
+
+    public function scopeActiveVips($query)
+    {
+        return $query->where('vip', true)->where('trial', false);
+    }
+    $activeVips = Contact::activeVips()->get();
+    */
 
 }
--- app/Http/Controllers/FacultyProgramController.php
+++ app/Http/Controllers/FacultyProgramController.php
@@ -3,16 +3,41 @@
 namespace App\Http\Controllers;
 
 use App\FacultyProgram;
+use League\Fractal\Manager;
+use League\Fractal\Resource\Collection;
+
+
 
 class FacultyProgramController extends Controller
 {
+    //$fractal = new Manager();
     public function __construct()
     {
         //$this->middleware('auth');
     }
 
     public function index(){
-        $tasks = FacultyProgram::all();
-        return $tasks;
+        $fractal = new Manager();
+        $facultyPrograms = FacultyProgram::all()->toArray();;
+
+        $resource = new Collection($facultyPrograms, function(array $facultyProgram) {
+            return [
+                'id'      => (int) $facultyProgram['id'],
+                'name'   => $facultyProgram['name'],
+                'faculty_id'    => (int) $facultyProgram['faculty_id'],
+                'allow_double_degree'    => (int) $facultyProgram['allow_double_degree'],
+                'is_regular'    => (boolean) $facultyProgram['is_regular'],
+                'min_points'    => (int) $facultyProgram['min_points'],
+                'created_at'    => (date_parse($facultyProgram['created_at'])),
+                'updated_at'    =>  (date_parse($facultyProgram['updated_at']))
+            ];
+        });
+
+        return $fractal->createData($resource)->toJson();
+    }
+
+    public function show($id){
+        $task = FacultyProgram::find($id);
+        return $task;
     }
 }
