367bb100ebeb84ccaa359d14e0e6283099e21c80
Denis Grabljevec <denis.grabljevec@siol.net>
2017-04-12 15:48:10 +0200

    Dodan module vue-Resource

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,9 +6,11 @@
 
 require('./bootstrap');
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 
 import routes from './routes'
 import AppView from './components/App.vue'
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -21,6 +23,8 @@ import AppView from './components/App.vue'
  * just import it here
  */
 Vue.use(VueRouter)
+Vue.use(VueResource)
+
 Vue.component('datatable', require('../../../node_modules/vuejs-datatable/src/vue-datatable.vue'))
 
 
