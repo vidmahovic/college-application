399ce11c910d3ca72e9d44c4811a889f734c29a3
Vid Mahovic <vid.mahovic@gmail.com>
2017-04-18 23:20:26 +0200

    Fix password generation problems and roles.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -19,39 +19,39 @@ class UsersTableSeeder extends Seeder
     {
         User::create([
             'email' => 'demo@demo.com',
-            'username' => 'demo',
+            'username' => 'demo123',
             'password' => app('hash')->make('demo123'),
-            'role_id' => 0
+            'role_id' => 1
         ]);
 
         User::create([
-            'email' => 'student1@student.mail.com',
+            'email' => 'student1@student.com',
             'username' => 'student1',
             'password' => app('hash')->make('student1'),
-            'role_id' => 0
+            'role_id' => 3
         ]);
 
         User::create([
-            'email' => 'student2@student.mail.com',
+            'email' => 'student2@student.com',
             'username' => 'student2',
             'password' => app('hash')->make('student2'),
-            'role_id' => 0
+            'role_id' => 3
         ]);
 
         User::create([
-            'email' => 'student3@student.mail.com',
+            'email' => 'student3@student.com',
             'username' => 'student3',
             'password' => app('hash')->make('student3'),
-            'role_id' => 0
+            'role_id' => 3
         ]);
 
 
 
         User::create([
             'email' => 'admin@admin.mail.com',
-            'username' => 'admin',
+            'username' => 'admin123',
             'password' => app('hash')->make('admin'),
-            'role_id' => 2
+            'role_id' => 1
         ]);
         
 
@@ -59,14 +59,14 @@ class UsersTableSeeder extends Seeder
             'email' => 'referent@fri.com',
             'username' => 'referent_fri',
             'password' => app('hash')->make('referentff'),
-            'role_id' => 1
+            'role_id' => 2
         ]);
 
         User::create([
             'email' => 'referent@ff.com',
             'username' => 'referent_ff',
             'password' => app('hash')->make('referentff'),
-            'role_id' => 1
+            'role_id' => 2
         ]);
     }
 
