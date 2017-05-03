7f1809c06ab1c7f777789698a22622da84e3441a
Nejc Ambrozic <nejc.ambrozic.slo@gmail.com>
2017-05-03 12:31:51 +0200

    added register form, redesigned login

--- resources/assets/js/components/Login.vue
+++ resources/assets/js/components/Login.vue
@@ -1,28 +1,84 @@
 <template>
-  <div class="container container-table">
-      <div class="row vertical-10p">
-        <div class="container">
-          <div class="text-center col-md-4 col-sm-offset-2">
-            <!-- errors -->
-            <!-- login form -->
-            <form class="ui form loginForm"  @submit.prevent="checkCreds">
-
-              <div class="input-group">
-                <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                <input class="form-control" name="username" placeholder="Username" type="text" v-model="username">
+  <div class="container">
+      <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <div class="panel panel-login">
+          <div class="panel-heading">
+            <div class="row">
+              <div class="col-xs-6">
+                <a href="" class="active" id="login-form-link">Prijava</a>
               </div>
-
-              <div class="input-group">
-                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
-                <input class="form-control" name="password" placeholder="Password" type="password" v-model="password">
+              <div class="col-xs-6">
+                <a href="" id="register-form-link">Registracija</a>
               </div>
-              <button type="submit" class="btn btn-primary">Login</button>
-              <p v-show="showResponse" class="bg-danger" style="padding-top: 10px; padding-bottom: 10px; margin-top: 15px;"> {{ response }} </p>
-            </form>
-
+            </div>
+            <hr>
+          </div>
+          <div class="panel-body">
+            <div class="row">
+              <div class="col-lg-12">
+                <form id="login-form" role="form" style="display: block;"  @submit.prevent="login">
+                  <div class="form-group">
+                    <input class="form-control" name="username" placeholder="Username" type="text" v-model="login.username" tabindex="1">
+                  </div>
+                  <div class="form-group">
+                    <input class="form-control" name="password" placeholder="Password" type="password" v-model="login.password" tabindex="2">
+                  </div>
+                  <p v-show="showResponse" class="bg-danger" style="padding-top: 10px; padding-bottom: 10px; margin-top: 15px;"> {{ response }} </p>
+                  <!--
+                  <div class="form-group text-center">
+                    <input type="checkbox" tabindex="3" class="" name="remember" id="remember">
+                    <label for="remember"> Remember Me</label>
+                  </div>
+                  -->
+                  <div class="form-group">
+                    <div class="row">
+                      <div class="col-sm-6 col-sm-offset-3">
+                        <input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Prijavi">
+                      </div>
+                    </div>
+                  </div>
+                  <!--
+                  <div class="form-group">
+                    <div class="row">
+                      <div class="col-lg-12">
+                        <div class="text-center">
+                          <a href="http://phpoll.com/recover" tabindex="5" class="forgot-password">Forgot Password?</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  -->
+                </form>
+                <form id="register-form" role="form" style="display: none;" @submit.prevent="register">
+                  <div class="form-group">
+                    <input type="text" v-model="reg.username" class="form-control" name="username" placeholder="Username"  tabindex="1">
+                    <!--<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">-->
+                  </div>
+                  <div class="form-group">
+                    <input type="email" v-model="reg.email" class="form-control" name="username" placeholder="Username" tabindex="1">
+                    <!-- <input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email Address" value="">-->
+                  </div>
+                  <div class="form-group">
+                    <input type="password" v-model="reg.pswd" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+                  </div>
+                  <div class="form-group">
+                    <input type="password" v-model="reg.repswd" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password">
+                  </div>
+                  <div class="form-group">
+                    <div class="row">
+                      <div class="col-sm-6 col-sm-offset-3">
+                        <input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Registriraj">
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+    </div>
   </div>
 </template>
 
@@ -34,35 +90,32 @@ module.exports = {
     return {
       section: 'Login',
       loading: '',
-      username: '',
-      password: '',
+      login: {
+        username: '',
+        password: ''
+      },
+      reg: {
+        username: '',
+        email: '',
+        pswd: '',
+        repswd: ''
+      },
       response: '',
       showResponse: false
     }
   },
   methods: {
-      checkCreds: function () {
-
-        // Mock user login..username => role: student | admin | referent | vpisna_sluzba
-
-        this.$http.post('api/login', {email: this.username, password: this.password})
+    login: function(apl) {
+        this.$http.post('api/login', {email: this.login.username, password: this.login.password})
           .then(function(res){
-            // let user = {
-            //   name: this.username,
-            //   loggedIn: true
-            // };
             let user = res.body.data
             this.$parent.user = user;
 
-            console.log(res);
-
+            
             window.localStorage.setItem('user', JSON.stringify(user));
             window.localStorage.setItem('token', res.body.meta.api_token);
 
             this.$router.push('/'+user.role);
-
-            // window.localStorage.setItem('user', user);
-            // window.localStorage.setItem('token', res.body.meta.api_token);
 
           }, function(err){
             this.showResponse = true;
@@ -73,13 +126,135 @@ module.exports = {
               this.response = "Uporabniško ime ali geslo ni pravilno.";
             }
           });
-
+      },
+      register: function() {
+        console.log(this.reg);
+      }
     },
     mounted() {
       console.log("login mounted")
     }
 
-  }
+  
 }
 
+
+$(function() {
+
+    $('#login-form-link').click(function(e) {
+    $("#login-form").delay(100).fadeIn(100);
+    $("#register-form").fadeOut(100);
+    $('#register-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+  $('#register-form-link').click(function(e) {
+    $("#register-form").delay(100).fadeIn(100);
+    $("#login-form").fadeOut(100);
+    $('#login-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+
+});
 </script>
+<style>
+  .panel-login {
+  border-color: #ccc;
+  -webkit-box-shadow: 0px 2px 3px 0px rgba(0,0,0,0.2);
+  -moz-box-shadow: 0px 2px 3px 0px rgba(0,0,0,0.2);
+  box-shadow: 0px 2px 3px 0px rgba(0,0,0,0.2);
+}
+.panel-login>.panel-heading {
+  color: #00415d;
+  background-color: #fff;
+  border-color: #fff;
+  text-align:center;
+}
+.panel-login>.panel-heading a{
+  text-decoration: none;
+  color: #666;
+  font-weight: bold;
+  font-size: 15px;
+  -webkit-transition: all 0.1s linear;
+  -moz-transition: all 0.1s linear;
+  transition: all 0.1s linear;
+}
+.panel-login>.panel-heading a.active{
+  color: #029f5b;
+  font-size: 18px;
+}
+.panel-login>.panel-heading hr{
+  margin-top: 10px;
+  margin-bottom: 0px;
+  clear: both;
+  border: 0;
+  height: 1px;
+  background-image: -webkit-linear-gradient(left,rgba(0, 0, 0, 0),rgba(0, 0, 0, 0.15),rgba(0, 0, 0, 0));
+  background-image: -moz-linear-gradient(left,rgba(0,0,0,0),rgba(0,0,0,0.15),rgba(0,0,0,0));
+  background-image: -ms-linear-gradient(left,rgba(0,0,0,0),rgba(0,0,0,0.15),rgba(0,0,0,0));
+  background-image: -o-linear-gradient(left,rgba(0,0,0,0),rgba(0,0,0,0.15),rgba(0,0,0,0));
+}
+.panel-login input[type="text"],.panel-login input[type="email"],.panel-login input[type="password"] {
+  height: 45px;
+  border: 1px solid #ddd;
+  font-size: 16px;
+  -webkit-transition: all 0.1s linear;
+  -moz-transition: all 0.1s linear;
+  transition: all 0.1s linear;
+}
+.panel-login input:hover,
+.panel-login input:focus {
+  outline:none;
+  -webkit-box-shadow: none;
+  -moz-box-shadow: none;
+  box-shadow: none;
+  border-color: #ccc;
+}
+.btn-login {
+  background-color: #59B2E0;
+  outline: none;
+  color: #fff;
+  font-size: 14px;
+  height: auto;
+  font-weight: normal;
+  padding: 14px 0;
+  text-transform: uppercase;
+  border-color: #59B2E6;
+}
+.btn-login:hover,
+.btn-login:focus {
+  color: #fff;
+  background-color: #53A3CD;
+  border-color: #53A3CD;
+}
+.forgot-password {
+  text-decoration: underline;
+  color: #888;
+}
+.forgot-password:hover,
+.forgot-password:focus {
+  text-decoration: underline;
+  color: #666;
+}
+
+.btn-register {
+  background-color: #1CB94E;
+  outline: none;
+  color: #fff;
+  font-size: 14px;
+  height: auto;
+  font-weight: normal;
+  padding: 14px 0;
+  text-transform: uppercase;
+  border-color: #1CB94A;
+}
+.btn-register:hover,
+.btn-register:focus {
+  color: #fff;
+  background-color: #1CA347;
+  border-color: #1CA347;
+}
+
+
+</style>
