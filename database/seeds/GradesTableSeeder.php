4d9f4c168bce5b99e8c5661049a253c5b2bb9d71
Rok Zidarn <rz3817@student.uni-lj.si>
2017-06-05 14:12:23 +0200

    Another seeder for testing

--- database/seeds/GradesTableSeeder.php
+++ database/seeds/GradesTableSeeder.php
@@ -37,5 +37,37 @@ class GradesTableSeeder extends Seeder
             'grade' => 5
         ]);
 
+        // ---
+
+        $grade = Grade::create([
+            'application_id' => 2,
+            'subject_id' => 'M103', // slo splosna
+            'grade' => 2
+        ]);
+
+        $grade = Grade::create([
+            'application_id' => 2,
+            'subject_id' => 'M401', // mat splosna
+            'grade' => 4
+        ]);
+
+        $grade = Grade::create([
+            'application_id' => 2,
+            'subject_id' => 'M241', // ang splosna
+            'grade' => 4
+        ]);
+
+        $grade = Grade::create([
+            'application_id' => 2,
+            'subject_id' => 'M431', // kem splosna
+            'grade' => 5
+        ]);
+
+        $grade = Grade::create([
+            'application_id' => 2,
+            'subject_id' => 'M541', // kem splosna
+            'grade' => 3
+        ]);
+
     }
 }
