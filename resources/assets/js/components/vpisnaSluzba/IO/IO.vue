7df9a2a13f87b5a4443e54eb19c65996840ad368
Nejc Ambrozic <nejc.ambrozic.slo@gmail.com>
2017-06-06 21:58:52 +0200

    upload files as base64 encoded strings

--- resources/assets/js/components/vpisnaSluzba/IO/IO.vue
+++ resources/assets/js/components/vpisnaSluzba/IO/IO.vue
@@ -6,21 +6,18 @@
               <div class="panel-heading">Uvoz podatkov</div>
 
               <div class="panel-body">
+                
+                <!-- splosna matura -->
                 <h3>Uvoz podatkov za splošno maturo</h3>
                 <div>
-                   
-                  <div >
-
-                  <!--<form method="post" action="/api/upload/general-matura" enctype="multipart/form-data"> -->
+                  <div>
                     <label> Maturanti</label>
-                      <input type="file" name="general_matura" @change="splosnaChange">
+                    <input type="file" name="general_matura" @change="splosnaChange">
                       
                     <label>Točke</label>
-                      <input type="file" name="general_matura_subjects" @change="splosnaChange_tocke">
+                    <input type="file" name="general_matura_subjects" @change="splosnaChange_tocke">
                   
-                    <!--<button type="submit" > Shrani</button>-->
-                    <div class="btn btn-success" v-on:click="submitSplosna">Uvozi</div>
-                  <!--</form>-->
+                    <div v-show="files.splosna != null && files.splosna_tocke != null" class="btn btn-success" v-on:click="submitSplosna">Uvozi</div>
                   </div>
                   <p v-show="msgs.splosna.display" v-bind:class="{'bg-danger': msgs.splosna.error, 'bg-success': !msgs.splosna.error}" style="padding: 10px; width: 30%; margin-top: 15px;">
                   {{ msgs.splosna.msg }}
@@ -28,12 +25,16 @@
                 </div>
 
                 <hr />
+                <!-- poklicna matura -->
                 <h3>Uvoz podatkoz za poklicno maturo</h3>
                 <div>
-                  <div class="input-group">
+                  <div>
+                    <label>Maturanti</label>
                     <input type="file" @change="poklicnaChange">
-                    <span class="input-group-addon" id="basic-addon2" style="padding:0px;">
-                    <div class="btn btn-success" v-on:click="submitPoklicna">Uvozi</div>
+
+                    <label>Točke</label>
+                    <input type="file" @change="poklicnaChange_tocke">                    
+                    <div v-show="files.poklicna != null && files.poklicna_tocke != null"  class="btn btn-success" v-on:click="submitPoklicna">Uvozi</div>
                     </span>
                   </div>
                 </div>
@@ -56,7 +57,7 @@
         files: {
           splosna: null,
           splosna_tocke: null,
-          poklica: null,
+          poklicna: null,
           poklicna_tocke: null
 
         },
@@ -111,12 +112,10 @@
         reader.readAsDataURL(file);
       },
       submitSplosna: function(status) {
-        console.log(this.files.splosna);
-
-        //todo kam treba postat
         
-        
-        this.$http.post("api/upload/general-matura", {'general_matura': this.files.splosna, "general_matura_subjects": this.files.splosna_tocke})
+        let splosna = this.files.splosna.split(",")[1];
+        let splosna_tocke = this.files.splosna_tocke.split(",")[1]
+        this.$http.post("api/upload/general-matura", {'general_matura': splosna, "general_matura_subjects": splosna_tocke})
         .then(function(data) {
         this.msgs.splosna.error = false;
             this.msgs.splosna.msg = "Podatki uspešno uvoženi";          
@@ -127,22 +126,24 @@
             this.msgs.splosna.msg = err.body.message;          
                 
           console.log(err);
-        })
+        });
       },
       submitPoklicna: function(status) {
         console.log(this.files.poklicna);
         //todo kam treba postat
-        // this.$http.post("api/", this.files.splosna)
-        // .then(function(data) {
-        // this.msgs.poklicna.error = false;
-            // this.msgs.poklicna.msg = "Podatki uspešno uvoženi";          
+        let poklicna = this.files.poklicna.split(",")[1];
+        let poklicna_tocke = this.files.poklicna_tocke.split(",")[1];
+        this.$http.post("api/upload/vocational-matura", {'vocational_matura': poklicna, 'vocational_matura_subjects': poklicna_tocke})
+        .then(function(data) {
+        this.msgs.poklicna.error = false;
+            this.msgs.poklicna.msg = "Podatki uspešno uvoženi";          
         
-        //   console.log(data),
-        // }, function(err) {
-            // this.msgs.poklicna.error = true;
-            // this.msgs.poklicna.msg = err.body.message;          
-        //   console.log(err);
-        // })
+          console.log(data);
+        }, function(err) {
+            this.msgs.poklicna.error = true;
+            this.msgs.poklicna.msg = err.body.message;          
+          console.log(err);
+        });
       }
      
     },
